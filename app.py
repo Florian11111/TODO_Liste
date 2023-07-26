e159ffdaeb6733a2717fb249b71df757334ec7ae
Florian11111 <116258400+Florian11111@users.noreply.github.com>
2023-07-26 23:13:37 +0200

    Flask server basis erstellt
    Der Flask server main file wurde erstellt. Dazu noch html file zum testen der Rest API und ein html file zum einlogen

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+from flask_cors import CORS
+import json
+
+app = Flask(__name__)
+CORS(app)
+
+with open('config.json', 'r') as config_file:
+    config = json.load(config_file)
+
+app.secret_key = config["secret_key"]
+SECRET_TOKEN = config["secret_token"]
+users = config["users"]
+
+# Route für die Anmeldeseite
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username in users and users[username]['password'] == password:
+            session['logged_in'] = True
+            session['username'] = username
+            return redirect(url_for('protected'))
+        else:
+            return 'Ungültige Anmeldedaten. Versuche es erneut.'
+    return render_template('login.html')
+
+# Route für die geschützte Seite
+@app.route('/protected')
+def protected():
+    if 'logged_in' in session and session['logged_in']:
+        return f'Willkommen, {session["username"]}! Dies ist die geschützte Seite.'
+    else:
+        return 'Du musst dich zuerst anmelden, um diese Seite zu sehen.'
+
+# Route für die Abmeldung
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
+
+# REST API TEIL --------------------------------------------------------------
+@app.route('/api/protected', methods=['GET'])
+def protected_api():
+    token = request.headers.get('Authorization')
+    print(token)
+    print(f"Bearer {SECRET_TOKEN}")
+
+    # Überprüfe, ob ein Token gesendet wurde und ob es dem erwarteten Token entspricht
+    if token and token == f"Bearer {SECRET_TOKEN}":
+        return jsonify({'message': 'Geschützte Ressource erfolgreich abgerufen'}), 200
+    else:
+        return jsonify({'message': 'Unautorisierter Zugriff'}), 401
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
